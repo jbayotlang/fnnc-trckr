ba79dab7bd774afe2d3a75186ea2a86ef3cdc2fd
Jhudiel Bayotlang <sbayotlang@gmail.com>
2024-04-26 17:18:51 +0800

    fix migrations

--- src/migrations/1714123101025-users.ts
+++ src/migrations/1714123101025-users.ts
@@ -1,12 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Users1714122463741 implements MigrationInterface {
-    name = 'Users1714122463741'
+export class Users1714123101025 implements MigrationInterface {
+    name = 'Users1714123101025'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user_profiles" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, CONSTRAINT "REL_8481388d6325e752cd4d7e26c6" UNIQUE ("userId"), CONSTRAINT "PK_1ec6662219f4605723f1e41b6cb" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying, "emailAddress" character varying, "password" character varying, "authMethod" "public"."users_authmethod_enum" NOT NULL DEFAULT 'local', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_61964cd8cb7b86e810f211f0cc9" UNIQUE ("emailAddress", "username"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "user_configs" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "setting" character varying NOT NULL, "value" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "userId" uuid, CONSTRAINT "REL_b2e94d5a023177b035defb5ba4" UNIQUE ("userId"), CONSTRAINT "PK_fc11c8861af6469fbd8920e9f80" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TYPE "public"."users_authmethod_enum" AS ENUM('local', 'google')`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying, "emailAddress" character varying, "password" character varying, "authMethod" "public"."users_authmethod_enum" NOT NULL DEFAULT 'local', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_61964cd8cb7b86e810f211f0cc9" UNIQUE ("emailAddress", "username"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "user_profiles" ADD CONSTRAINT "FK_8481388d6325e752cd4d7e26c6d" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "user_configs" ADD CONSTRAINT "FK_b2e94d5a023177b035defb5ba4a" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
@@ -14,8 +15,9 @@ export class Users1714122463741 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "user_configs" DROP CONSTRAINT "FK_b2e94d5a023177b035defb5ba4a"`);
         await queryRunner.query(`ALTER TABLE "user_profiles" DROP CONSTRAINT "FK_8481388d6325e752cd4d7e26c6d"`);
-        await queryRunner.query(`DROP TABLE "user_configs"`);
         await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TYPE "public"."users_authmethod_enum"`);
+        await queryRunner.query(`DROP TABLE "user_configs"`);
         await queryRunner.query(`DROP TABLE "user_profiles"`);
     }
 
